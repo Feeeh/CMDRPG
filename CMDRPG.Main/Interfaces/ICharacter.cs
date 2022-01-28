74a173081241c838b6f1a0b32d2b4e022db0a278
Felipe G. Brandão <feeeh@gf2.com.br>
2022-01-28 17:00:27 -0300

    Initial commit

---
+++ CMDRPG.Main/Interfaces/ICharacter.cs
@@ -0,0 +1,10 @@
+﻿namespace CMDRPG.Main.Interfaces
+{
+    public interface ICharacter
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Inteligence { get; set; }
+        public int Evasion { get; set; }
+    }
+}
---
+++ CMDRPG.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMDRPG.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
